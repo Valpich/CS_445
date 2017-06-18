6bf21a4efe13e2fd1650b33fe1fefdf5bd653894
Valentin Pichavant <valentinpichavant@Valentins-MBP.lan>
2017-06-18 18:04:10 -0500

    OrderedProduct add

---
+++ src/main/java/iit/cs445/models/products/Ordered_Products.java
@@ -0,0 +1,9 @@
+package iit.cs445.models.products;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ordered_products")
+public class Ordered_Products {
+}
