da1015157d46bdf62030609e494d6b2effd4d6e9
Valentin Pichavant <valentinpichavant@valentins-mbp.lan>
2017-06-26 16:48:27 -0500

    Add test classes

---
+++ src/test/java/iit/cs445/models/users/UserManagerTest.java
@@ -0,0 +1,22 @@
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by valentinpichavant on 6/26/17.
+ */
+@RunWith(Arquillian.class)
+public class UserManagerTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(iit.cs445.models.users.UserManager.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+}
