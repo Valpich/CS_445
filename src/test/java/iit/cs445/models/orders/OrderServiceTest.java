c08c7210c65a65793ed04ccb8cbb7738003316e5
Valentin Pichavant <valentinpichavant@valentins-mbp.lan>
2017-06-26 18:33:09 -0500

    Debug test

--- src/test/java/iit/cs445/models/orders/OrderServiceTest.java
+++ src/test/java/iit/cs445/models/orders/OrderServiceTest.java
@@ -15,30 +15,33 @@ import static org.junit.Assert.*;
 @RunWith(Arquillian.class)
 public class OrderServiceTest {
 
-    private class OrderSerivceImplementTest implements OrderService{}
+    private class OrderServiceImplementTest implements OrderService{}
 
-    private OrderSerivceImplementTest orderSerivceImplementTest;
+    private OrderServiceImplementTest orderServiceImplementTest;
 
     @Before
     public void setUp() throws Exception {
-        orderSerivceImplementTest = new OrderSerivceImplementTest();
+        orderServiceImplementTest = new OrderServiceImplementTest();
     }
 
     @After
     public void tearDown() throws Exception {
-        orderSerivceImplementTest = null;
+        orderServiceImplementTest = null;
     }
 
     @Test
     public void createNewOrder() throws Exception {
+        assertEquals(null, orderServiceImplementTest.createNewOrder(new Order()));
     }
 
     @Test
     public void updateOrder() throws Exception {
+        assertEquals(null, orderServiceImplementTest.updateOrder(new Order()));
     }
 
     @Test
     public void deleteOrder() throws Exception {
+        assertEquals(null, orderServiceImplementTest.deleteOrder(new Order()));
     }
 
     @Deployment
