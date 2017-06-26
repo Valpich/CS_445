b8e52ff7b3efd1fbeb40111e2f5771db576d1126
Valentin Pichavant <valentinpichavant@valentins-mbp.lan>
2017-06-25 20:38:05 -0500

    Pass orders

---
+++ src/main/java/iit/cs445/models/users/OrderAddress.java
@@ -0,0 +1,183 @@
+package iit.cs445.models.users;
+
+import iit.cs445.models.BaseEntity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "addresses")
+public class Address extends BaseEntity<Long, Address> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "address_id")
+    private Long id;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "street_address")
+    private String streetAddress;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_address", joinColumns = {
+            @JoinColumn(name = "address_id", nullable = false, updatable = false) },
+            inverseJoinColumns = { @JoinColumn(name = "user_id",
+                    nullable = false, updatable = false) })
+    private User user;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", streetAddress='" + streetAddress + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                "} " + super.toString();
+    }
+
+    @Override
+    public Address clone() {
+        Address address = new Address();
+        address.setPhoneNumber(phoneNumber);
+        address.setStreetAddress(streetAddress);
+        address.setFirstName(firstName);
+        address.setLastName(lastName);
+        address.setZipCode(zipCode);
+        address.setCountry(country);
+        address.setCity(city);
+        address.setState(state);
+        return address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Address address = (Address) o;
+
+        if (id != null ? !id.equals(address.id) : address.id != null) return false;
+        if (phoneNumber != null ? !phoneNumber.equals(address.phoneNumber) : address.phoneNumber != null) return false;
+        if (streetAddress != null ? !streetAddress.equals(address.streetAddress) : address.streetAddress != null)
+            return false;
+        if (firstName != null ? !firstName.equals(address.firstName) : address.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(address.lastName) : address.lastName != null) return false;
+        if (zipCode != null ? !zipCode.equals(address.zipCode) : address.zipCode != null) return false;
+        if (country != null ? !country.equals(address.country) : address.country != null) return false;
+        if (city != null ? !city.equals(address.city) : address.city != null) return false;
+        return state != null ? state.equals(address.state) : address.state == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
+        result = 31 * result + (streetAddress != null ? streetAddress.hashCode() : 0);
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (zipCode != null ? zipCode.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (state != null ? state.hashCode() : 0);
+        return result;
+    }
+
+}
