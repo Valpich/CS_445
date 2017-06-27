b9a0976d4ae7c8f243842a628513252cbef8cec9
Valentin Pichavant <valentinpichavant@valentins-mbp.lan>
2017-06-26 19:26:59 -0500

    Order testing

--- src/test/java/iit/cs445/models/orders/OrderTest.java
+++ src/test/java/iit/cs445/models/orders/OrderTest.java
@@ -1,5 +1,10 @@
 package iit.cs445.models.orders;
 
+import iit.cs445.models.products.Product;
+import iit.cs445.models.services.Service;
+import iit.cs445.models.users.Address;
+import iit.cs445.models.users.OrderAddress;
+import iit.cs445.models.users.User;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -9,6 +14,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -24,58 +32,121 @@ public class OrderTest {
 
     @After
     public void tearDown() throws Exception {
+        order = null;
     }
 
     @Test
     public void getId() throws Exception {
+        order.setId(2L);
+        assertEquals(2L, order.getId().longValue());
     }
 
     @Test
     public void setId() throws Exception {
+        order.setId(2L);
+        assertEquals(2L, order.getId().longValue());
+        order.setId(3L);
+        assertEquals(3L, order.getId().longValue());
     }
 
     @Test
     public void getStatus() throws Exception {
+        order.setStatus(OrderStatus.PROCESSING);
+        assertEquals(OrderStatus.PROCESSING, order.getStatus());
     }
 
     @Test
     public void setStatus() throws Exception {
+        order.setStatus(OrderStatus.PROCESSING);
+        assertEquals(OrderStatus.PROCESSING, order.getStatus());
+        order.setStatus(OrderStatus.SHIPPED);
+        assertEquals(OrderStatus.SHIPPED, order.getStatus());
     }
 
     @Test
     public void getOrderedProducts() throws Exception {
+        List<Product> orderedProducts = new ArrayList<>();
+        order.setOrderedProducts(orderedProducts);
+        assertEquals(orderedProducts, order.getOrderedProducts());
     }
 
     @Test
     public void setOrderedProducts() throws Exception {
+        List<Product> orderedProducts = new ArrayList<>();
+        order.setOrderedProducts(orderedProducts);
+        assertEquals(orderedProducts, order.getOrderedProducts());
+        List<Product> orderedProductsTwo = new ArrayList<>();
+        order.setOrderedProducts(orderedProductsTwo);
+        assertEquals(orderedProductsTwo, order.getOrderedProducts());
     }
 
     @Test
     public void getOrderedServices() throws Exception {
+        List<Service> orderedServices = new ArrayList<>();
+        order.setOrderedServices(orderedServices);
+        assertEquals(orderedServices, order.getOrderedServices());
     }
 
     @Test
     public void setOrderedServices() throws Exception {
+        List<Service> orderedServices = new ArrayList<>();
+        order.setOrderedServices(orderedServices);
+        assertEquals(orderedServices, order.getOrderedServices());
+        List<Service> orderedServicesTwo = new ArrayList<>();
+        order.setOrderedServices(orderedServicesTwo);
+        assertEquals(orderedServicesTwo, order.getOrderedServices());
     }
 
     @Test
     public void getOrderAddress() throws Exception {
+        OrderAddress address = new OrderAddress();
+        order.setOrderAddress(address);
+        assertEquals(address, order.getOrderAddress());
     }
 
     @Test
     public void setOrderAddress() throws Exception {
+        OrderAddress address = new OrderAddress();
+        order.setOrderAddress(address);
+        assertEquals(address, order.getOrderAddress());
+        OrderAddress addressTwo = new OrderAddress();
+        order.setOrderAddress(addressTwo);
+        assertEquals(addressTwo, order.getOrderAddress());
     }
 
     @Test
     public void getUser() throws Exception {
+        User user = new User();
+        order.setUser(user);
+        assertEquals(user, order.getUser());
     }
 
     @Test
     public void setUser() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        order.setUser(user);
+        assertEquals(user, order.getUser());
+        User userTwo = new User();
+        userTwo.setId(2L);
+        order.setUser(userTwo);
+        assertEquals(userTwo, order.getUser());
     }
 
     @Test
     public void toStringTest() throws Exception {
+        order.setId(2L);
+        order.setStatus(OrderStatus.PROCESSING);
+        List<Product> orderedProducts = new ArrayList<>();
+        order.setOrderedProducts(orderedProducts);
+        List<Service> orderedServices = new ArrayList<>();
+        order.setOrderedServices(orderedServices);
+        OrderAddress address = new OrderAddress();
+        order.setOrderAddress(address);
+        User user = new User();
+        user.setId(1L);
+        order.setUser(user);
+        assertNotNull(order.toString());
     }
 
     @Deployment
